dc5604d6453ef21cdee18e90ce67c0942f5c2f71
tuomaskivioja <tuomas@tuomaskivioja.com>
2023-05-26 18:02:54 +0300

    sessions

--- todo/views.py
+++ todo/views.py
@@ -5,13 +5,7 @@ from .forms import TaskForm
 
 # Create your views here.
 
-tasks = [
-    {"id": 1, "name": "Make video", "description": "make vid for course", "priority": 1},
-    {"id": 2, "name": "Do dishes", "description": "wash dishes", "priority": 2},
-    {"id": 3, "name": "Call mum", "description": "call mum fast!!!", "priority": 3}
-]
-
-def get_task_by_id(task_id):
+def get_task_by_id(tasks,task_id):
     for task in tasks:
         if task["id"] == task_id:
             return task
@@ -19,28 +13,27 @@ def get_task_by_id(task_id):
 
 def index(request):
     task_form = TaskForm()
-    ctx = {"tasks": tasks, "form": task_form }
-    print("index", tasks)
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    ctx = {"tasks": request.session["tasks"], "form": task_form }
     return render(request, "todo/index.html", ctx)
 
 def add_task(request):
-    print(request.POST)
+    request.session["hello"] = "world"
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             task = {
-                "id": len(tasks) + 1,
+                "id": len(request.session["tasks"]) + 1,
                 "name": form.cleaned_data['task'],
                 "description": form.cleaned_data['description'],
                 "priority": int(form.cleaned_data['priority']),
             }
-            print(task)
-            tasks.append(task)
-            print(tasks)
+            request.session["tasks"].append(task)
     return HttpResponseRedirect(reverse('todo:index'))
 
 def task(request, id):
-    ctx = {"task": get_task_by_id(id)}
+    ctx = {"task": get_task_by_id(request.session['tasks'], id)}
     return render(request, "todo/task.html", ctx)
 
 class AboutView(View):
