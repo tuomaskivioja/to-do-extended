70a34475bf657fefc0fddab5006f05b5a7412b91
tuomaskivioja <tuomas@tuomaskivioja.com>
2023-05-27 18:59:34 +0300

    models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -13,27 +14,25 @@ def get_task_by_id(tasks,task_id):
 
 def index(request):
     task_form = TaskForm()
-    if "tasks" not in request.session:
-        request.session["tasks"] = []
-    ctx = {"tasks": request.session["tasks"], "form": task_form }
+    tasks = Task.objects.all()
+    ctx = {"tasks": tasks, "form": task_form}
     return render(request, "todo/index.html", ctx)
 
 def add_task(request):
-    request.session["hello"] = "world"
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = {
-                "id": len(request.session["tasks"]) + 1,
-                "name": form.cleaned_data['task'],
-                "description": form.cleaned_data['description'],
-                "priority": int(form.cleaned_data['priority']),
-            }
-            request.session["tasks"].append(task)
+            task = Task(
+                name=form.cleaned_data['task'],
+                description=form.cleaned_data['description'],
+                priority=int(form.cleaned_data['priority'])
+            )
+            task.save()
     return HttpResponseRedirect(reverse('todo:index'))
 
 def task(request, id):
-    ctx = {"task": get_task_by_id(request.session['tasks'], id)}
+    task = Task.objects.get(id=id)
+    ctx = {"task": task}
     return render(request, "todo/task.html", ctx)
 
 class AboutView(View):
